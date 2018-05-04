36ffd606be228db879c4b594104a8c7aa03d35a9
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-05-04 18:10:00 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,3 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray (array, element) {chocolateBars.unshift(element)}
+function destructivelyAddElementToBeginningOfArray (array, element) {chocolateBars.unshift(element)}
